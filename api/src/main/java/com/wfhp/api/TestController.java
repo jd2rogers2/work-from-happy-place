b46fc132eeecad8bc1cee5e83b01880d5996e8fc
james rogers <jd2rogers2@gmail.com>
2024-09-04 15:46:43 -0700

    springbooted a java api, can hit test endpoint on 8080

---
+++ api/src/main/java/com/wfhp/api/TestController.java
@@ -0,0 +1,13 @@
+package com.wfhp.api;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class TestController {
+
+  @RequestMapping("/test")
+  public String test() {
+    return "test 1";
+  }
+}
